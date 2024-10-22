1a8b28fb3bb0a4341124add7f18666b2c2e27702
Orest Palii <paliy.o.2004@gmail.com>
2024-10-22 18:44:45 +0300

    init commit

---
+++ MoviewLibrary/MoviewLibrary/MoviewLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviewLibraryApp.swift
+//  MoviewLibrary
+//
+//  Created by Orest Palii on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviewLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
